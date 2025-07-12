0844ac832302eaafb1422c2dbfb566082b310287
IStasovskyi93 <stsovsky400m@gmail.com>
2025-07-12 19:04:36 +0200

    make digit in icon when I add product to cart

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -18,11 +18,17 @@ export const CartSlice = createSlice({
         // If item does not exist, add it to the cart with quantity 1
         state.items.push({ name, image, cost, quantity: 1 });
       }
+      state.numOfItems += 1;
     },
     removeItem: (state, action) => {
       const { name, quantity } = action.payload;
       state.items = state.items.filter((item) => item.name !== name);
       state.numOfItems -= quantity;
+
+      // Just to be sure... I hate negative numbers
+      if (state.numOfItems < 0) {
+        state.numOfItems = 0;
+      }
     },
     updateQuantity: (state, action) => {
       const { name, quantity } = action.payload; // Destructure the product name and new quantity from the action payload
